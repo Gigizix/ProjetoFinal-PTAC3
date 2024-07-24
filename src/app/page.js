a7c35b600f1f19bea91e1ffec4688f26f258f978
Gigizix <131817471+Gigizix@users.noreply.github.com>
2024-07-24 02:06:29 -0400

    Adicionado useEffect e useEstado(falta terminar)

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,35 @@
+'use client'
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import "./styles.css";
-import Link from "next/link";
+import Carregando from "../app/Componentes/Carregando";
+import ErrorGetData from "../app/Componentes/ErrorGetData";
 
 export default function Home() {
 
-  return (
-    <main>
-     
-    </main>
-  );
-}
+  const [listaProduct, setListaProdudt] = useState ([]);
+  const [listaComplete, setListaComplete] = useState ([]);
+  const [search, setSearch] = useState("");
+  const [errorFatch, setErrorFatch] = useState (false);
+
+  useEffect(() => {
+    const getProtucts = async () =>{
+      try {
+        const response = await fetch("http://localhost:3000/api")
+        const data = await response.json();
+        setListaProdudt(data);
+        setListaComplete(data);
+      }catch{
+        setErrorFatch(true);
+      }
+    };
+    getProtucts();
+  }, []);
+
+  const orderAz  = () => {
+    let newList = [...listaProduct].sort((a,b)=>
+      a.bolsa.localeCompare(b.bolsa)
+    );
+    setListaProdudt(newList);
+  }
+
+  
