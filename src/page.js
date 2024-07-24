2ed0dfd64ca90204bb2ea96ef2c51ece677f54df
Gigizix <131817471+Gigizix@users.noreply.github.com>
2024-07-24 13:54:58 -0400

    Atualizar o page.js da pasta relógio

---
+++ src/page.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import styles from "./relogio.module.css"
+import Link from "next/link"
+export default async function Relogio{ params }) {
+
+    const response = await fetch("http://localhost:3000/api/" + params.id);
+
+    const data = await response.json();
+    return (
+        <div key={data.id}>
+            <br />
+            <div className={styles.container}>
+                <p className={styles.relogio}>{data.relogio}</p>
+                <div>
+                    <Image
+                        width={300}
+                        height={300}
+                        src={data.img} />
+                </div>
+                <p className={styles.preco}>R${data.preco}</p>
+                <p className={styles.cor}>{data.cor}</p>
+                <p className={styles.tam}>{data.tamanho}</p>
+                <Link className={styles.link} href="/">
+            Voltar
+            </Link>
+            </div>
+        </div>
+    );
+}
