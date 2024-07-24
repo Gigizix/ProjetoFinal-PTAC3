f1a8d30ff3faf7af71a8d5d397c47df5a5991893
Gigizix <131817471+Gigizix@users.noreply.github.com>
2024-07-24 01:55:14 -0400

    useEffect e useEstado

---
+++ src/Rodapé.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Footer(){
+    return(
+        <footer>
+            <nav> 
+                <Image
+                     width={50}
+                     height={50}
+                     src= "https://tse2.mm.bing.net/th?id=OIP.E0fJoebFtBmzdNibqJA8egHaGB&pid=Api&P=0&h=180"
+                />
+                 
+                 <Image
+                     width={50}
+                     height={50}
+                     src= "https://tse1.mm.bing.net/th?id=OIP.lQRk_LuIIh-6LXK9jplxGAHaHZ&pid=Api&P=0&h=180"
+                />
+
+                 <Image
+                     width={50}
+                     height={50}
+                     src= https://tse1.mm.bing.net/th?id=OIP.aB8Zep1Uajjii9WnIbJw7QHaHa&pid=Api&rs=1&c=1&qlt=95&w=122&h=122"
+                />
+
+                 <Image
+                     width={50}
+                     height={50}
+                     src= "https://tse2.mm.bing.net/th?id=OIP.bD6otkD2EDUCHCDdXc03-gHaGK&pid=Api&P=0&h=180"
+                />
+
+                 <Image
+                     width={50}
+                     height={50}
+                    src= "https://tse1.mm.bing.net/th?id=OIP.Beseg65o_c_Qe4m8XzP5iwHaFZ&pid=Api&P=0&h=180"
+                />
+            </nav>
+        </footer>
+    )
+}
